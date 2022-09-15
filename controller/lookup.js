e12cf9d2850f349e87863e659ed0db3bc0191b8d
kchiao88 <kazchiao@gmail.com>
2022-09-14 18:58:58 -0700

    processed lookupcallsign

--- controller/lookup.js
+++ controller/lookup.js
@@ -1,4 +1,4 @@
-
+const fs = require('fs');
 
 const lookupLogic = {};
 async function processLookupCallsign(req, res, next) {
@@ -6,9 +6,27 @@ async function processLookupCallsign(req, res, next) {
     // TODO: Use the callsign variable to obtain data of callsigns related to user input.
     // NOTE: variable 'callsign' contains user input from the online form in "lookup.hbs."
     console.log(callsign); 
+    let data = await fetch(`https://callook.info/${callsign}/json`)
+        .then(response => response.json())
+    if (data.status === "INVALID") {
+        res.send("Not found")
+    }
+    const fileName = "./licenses/" + callsign + ".json";
+    fs.writeFile(fileName, JSON.stringify(data), (error)=>{
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Success");
+        }
+    })
 
     //res.render() //unlock once we create a results page. 
-    res.send(callsign); 
+    res.send(data); 
 }
 lookupLogic.processLookupCallsign = processLookupCallsign;
+async function processDeleteCallsign(req, res, next) {
+    callsign = req.body.callsign;
+    // TODO: Use fs to delete the file
+}
+lookupLogic.processDeleteCallsign = processDeleteCallsign;
 module.exports = lookupLogic;
