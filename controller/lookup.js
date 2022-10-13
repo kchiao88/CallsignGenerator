3fd7d4869af732bda8e119e8506fc16bae9d8d17
kchiao88 <kazchiao@gmail.com>
2022-10-12 18:44:36 -0700

    optimization done, erroneous for illogical callsigns

--- controller/lookup.js
+++ controller/lookup.js
@@ -3,14 +3,82 @@ const fs = require('fs');
 const lookupLogic = {};
 async function processLookupCallsign(req, res, next) {
     // *TODO*: make function better by looking if callsign exists in file system
-    callsign = req.body.callsign; 
+    callsign = req.body.callsign; // coming from lookup.hbs (user input)
     // NOTE: variable 'callsign' contains user input from the online form in "lookup.hbs."
     console.log(callsign); 
+    const path = "./licenses/" + callsign + ".json";
+    if (fs.exists(path, (isExist) => {
+        if (isExist) {
+            console.log("File already exists")
+        } else {
+            console.log("File doesn't exist")
+            next()
+        }
+    } )) {
+        console.log("Case 1")
+        fs.readFile(path, (error, content) => {
+            if (error) {
+                console.log("Error reading the file")
+            } else {
+                const data = JSON.parse(content);
+                console.log(data)
+                res.render("display", {callsign: callsign, 
+                    latitude: data.location.latitude, 
+                    longitude: data.location.longitude,
+                    status: data.status,
+                    class: data.current.operClass,
+                    name: data.name,
+                    frn: data.otherInfo.frn,
+                    address: data.address.line2,
+                    grid: data.location.gridsquare,
+                    grantDate: data.otherInfo.grantDate,
+                    expiryDate: data.otherInfo.expiryDate,
+                    url: data.otherInfo.ulsUrl
+                }); 
+            }
+        })
+    } else {
+        console.log("Case 2")
+        let data = await fetch(`https://callook.info/${callsign}/json`)
+        .then(response => response.json())
+        if (data.status === "INVALID") {
+            // res.send("Not found")
+            next()
+        } else {
+            // Creating new JSON file to store callsign data 
+            const fileName = "./licenses/" + callsign + ".json";
+            fs.writeFile(fileName, JSON.stringify(data), (error)=>{
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log("Success");
+                }
+            })
+
+            // Pulling callsign information from data variable
+            res.render("display", {callsign: callsign, 
+                latitude: data.location.latitude, 
+                longitude: data.location.longitude,
+                status: data.status,
+                class: data.current.operClass,
+                name: data.name,
+                frn: data.otherInfo.frn,
+                address: data.address.line2,
+                grid: data.location.gridsquare,
+                grantDate: data.otherInfo.grantDate,
+                expiryDate: data.otherInfo.expiryDate,
+                url: data.otherInfo.ulsUrl
+            }); 
+        }
+
+    }
+    // fetching callsign data from site
     let data = await fetch(`https://callook.info/${callsign}/json`)
         .then(response => response.json())
     if (data.status === "INVALID") {
         res.send("Not found")
     }
+    // Creating new JSON file to store callsign data 
     const fileName = "./licenses/" + callsign + ".json";
     fs.writeFile(fileName, JSON.stringify(data), (error)=>{
         if (error) {
@@ -20,7 +88,7 @@ async function processLookupCallsign(req, res, next) {
         }
     })
 
-    //res.render() //unlock once we create a results page. 
+    // Pulling callsign information from data variable
     res.render("display", {callsign: callsign, 
         latitude: data.location.latitude, 
         longitude: data.location.longitude,
