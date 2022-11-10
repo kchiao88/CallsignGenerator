7dd5518f9e91bf9c3f2dc9c6621a907cf9ee5d9d
kchiao88 <kazchiao@gmail.com>
2022-11-09 19:07:40 -0800

    added registration route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'RadioCallsignGenerator', bodyIndex: "Hello Kaz, and John." });
 });
 
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'Registration'})
+})
+
+router.post('/registration', function(req, res, next) {
+  console.log(req.body.username)
+next() })
+
 module.exports = router;
