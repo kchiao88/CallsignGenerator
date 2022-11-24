72415ecbcd0bb6e299c52f251d509c2da681e48d
kchiao88 <kazchiao@gmail.com>
2022-11-23 19:01:52 -0800

    in the process of finishing registration

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,19 @@ router.get('/registration', function(req, res, next) {
 })
 
 router.post('/registration', function(req, res, next) {
-  console.log(req.body.username)
-next() })
+  const username = req.body.username
+  const password = req.body.password
+  const emailAddress = req.body.email
+  const accessKey = req.body.key
+  const emailRe = /^\w+@([a-zA-Z_]|[0-9])+?\.[a-zA-Z]{2,3}$/
+  const emailCheck = emailAddress.match(emailRe)
+  console.log(emailCheck)
+  if (emailCheck) {
+    res.send("Success")
+  } else {
+    res.send("Failure")
+    // To-Do: Add middleware that warns people
+  }
+})
 
 module.exports = router;
