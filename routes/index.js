64d4b637e3e258ed7f5bbfbce74a2a746fe96c7a
jsanjose-sfsu <jsanjose@mail.sfsu.edu>
2022-12-08 11:09:43 -0800

    allowing us the ability to sequalize our data and do migrations.

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/registration', function(req, res, next) {
-  res.render('registration', { title: 'Registration'})
+  res.render('registration', { title: 'Registration'}); 
 })
 
 router.post('/registration', function(req, res, next) {
-  const username = req.body.username
-  const password = req.body.password
-  const emailAddress = req.body.email
-  const accessKey = req.body.key
-  const emailRe = /^\w+@([a-zA-Z_]|[0-9])+?\.[a-zA-Z]{2,3}$/
-  const emailCheck = emailAddress.match(emailRe)
-  console.log(emailCheck)
-  const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/
-  const passwordCheck = password.match(passwordRe)
-  console.log(passwordCheck)
+  
+  const username = req.body.username; 
+  const password = req.body.password; 
+  const emailAddress = req.body.email; 
+  const accessKey = req.body.key; 
+  const emailRe = /^\w+@([a-zA-Z_]|[0-9])+?\.[a-zA-Z]{2,3}$/; 
+  const emailCheck = emailAddress.match(emailRe); 
+  const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/; 
+  const passwordCheck = password.match(passwordRe); 
+
   if (passwordCheck) {
-    res.send("Success")
+    res.send(process.env.DATABASE_URL); 
   } else {
-    res.send("Failure")
+    res.send("Failure"); 
   }
 })
 
