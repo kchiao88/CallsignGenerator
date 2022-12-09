f15249df23f9d8b7b1c9e7cd0f973bed6f48c9fd
kchiao88 <kazchiao@gmail.com>
2022-12-08 17:58:46 -0800

    updated work

--- controller/index.js
+++ controller/index.js
@@ -3,3 +3,23 @@ const fs = require('fs');
 const indexLogic = {};
 
 // TODO: Create the middleware logic for the login, logout and registration mechanic here.
+
+function processRegistration(req, res, next) {
+    const username = req.body.username; 
+    const password = req.body.password; 
+    const emailAddress = req.body.email; 
+    const accessKey = req.body.key; 
+    const emailRe = /^\w+@([a-zA-Z_]|[0-9])+?\.[a-zA-Z]{2,3}$/; 
+    const emailCheck = emailAddress.match(emailRe); 
+    const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/; 
+    const passwordCheck = password.match(passwordRe); 
+
+    if (passwordCheck) {
+        res.send("Hello World"); 
+    } else {
+        res.send("Failure"); 
+    }
+}
+
+indexLogic.processRegistration = processRegistration;
+module.exports = indexLogic;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var indexLogic = require('../controller/index')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'RadioCallsignGenerator', bodyIndex: "Hello Kaz, and John." });
@@ -10,22 +10,8 @@ router.get('/registration', function(req, res, next) {
   res.render('registration', { title: 'Registration'}); 
 })
 
-router.post('/registration', function(req, res, next) {
-  
-  const username = req.body.username; 
-  const password = req.body.password; 
-  const emailAddress = req.body.email; 
-  const accessKey = req.body.key; 
-  const emailRe = /^\w+@([a-zA-Z_]|[0-9])+?\.[a-zA-Z]{2,3}$/; 
-  const emailCheck = emailAddress.match(emailRe); 
-  const passwordRe = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/; 
-  const passwordCheck = password.match(passwordRe); 
-
-  if (passwordCheck) {
-    res.send(process.env.DATABASE_URL); 
-  } else {
-    res.send("Failure"); 
-  }
-})
+router.post('/registration', 
+  indexLogic.processRegistration
+)
 
 module.exports = router;
